9f6ecc7d0647be09123602583694dec71ffa0a89
Fedevvi <federica@belkadigital.com>
2024-05-03 11:14:34 +0200

    chore: remove useless imports

--- src/components/Title/types.ts
+++ src/components/Title/types.ts
@@ -1,7 +1,4 @@
-import { VariantProps } from "class-variance-authority";
-
 import type { ComponentPropsWithoutRef, ElementRef } from "react";
-import { titleStyles } from "./styles";
 
 type TitleComponent = "h1" | "h2" | "h3" | "h4";
 type TitleElement = ElementRef<TitleComponent>;
